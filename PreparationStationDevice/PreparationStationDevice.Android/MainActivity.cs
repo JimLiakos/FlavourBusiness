8cfb4a5eabf7c81fa8aad53ea9fa9c076a5e2182
Jim Liakos <Jim.Liakos.Msdn@hotmail.com>
2021-07-14 20:03:10 +0300

    jim

--- PreparationStationDevice/PreparationStationDevice.Android/MainActivity.cs
+++ PreparationStationDevice/PreparationStationDevice.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.OS;
 namespace PreparationStationDevice.Droid
 {
     [Activity(Label = "PreparationStationDevice", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity :OOAdvantech.Droid.SpeechActivity // global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
--- PreparationStationDevice/PreparationStationDevice/MainPage.xaml.cs
+++ PreparationStationDevice/PreparationStationDevice/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOAdvantech.Speech;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -28,14 +29,39 @@ namespace PreparationStationDevice
 
             url =string.Format( @"http://{0}:4301/", FlavourBusinessFacade.ComputingResources.EndPoint.Server);//work
             hybridWebView.Uri = url;
+
+            try
+            {
+                _speechRecongnitionInstance = DependencyService.Get<ISpeechText>();
+                _speechRecongnitionInstance.SpeechRecognized += _speechRecongnitionInstance_SpeechRecognized;
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
+        private void _speechRecongnitionInstance_SpeechRecognized(List<string> speechTexts)
+        {
+            //SpeechText = "";
+            //foreach (string text in speechTexts)
+            //    SpeechText = text;
+
+
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SpeechText)));
+        }
+
+        private ISpeechText _speechRecongnitionInstance;
         protected override async void OnAppearing()
         {
             base.OnAppearing();
             var cameraPermission = await Xamarin.Essentials.Permissions.CheckStatusAsync<Xamarin.Essentials.Permissions.Camera>();
             if (cameraPermission != Xamarin.Essentials.PermissionStatus.Granted)
                 cameraPermission = await Xamarin.Essentials.Permissions.RequestAsync<Xamarin.Essentials.Permissions.Camera>();
+
+            var micPermission = await Xamarin.Essentials.Permissions.CheckStatusAsync<Xamarin.Essentials.Permissions.Microphone>();
+            if (micPermission != Xamarin.Essentials.PermissionStatus.Granted)
+                micPermission = await Xamarin.Essentials.Permissions.RequestAsync<Xamarin.Essentials.Permissions.Microphone>();
 
         }
     }
