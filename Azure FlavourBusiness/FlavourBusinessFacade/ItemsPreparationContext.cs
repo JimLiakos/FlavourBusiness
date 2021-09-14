444080b70a2682e183334da16d2e0a06069a197a
Jim Liakos <Jim.Liakos.Msdn@hotmail.com>
2021-09-14 13:04:49 +0300

    jim

---
+++ Azure FlavourBusiness/FlavourBusinessFacade/ItemsPreparationContext.cs
@@ -0,0 +1,29 @@
+using FlavourBusinessFacade.ServicesContextResources;
+using System.Collections.Generic;
+
+namespace FlavourBusinessFacade.RoomService
+{
+    /// <MetaDataID>{d83d4466-195d-471e-be61-fdceb04c166e}</MetaDataID>
+    public class ItemsPreparationContext
+    {
+        
+        public string PreparationStationIdentity { get; set; }
+        public List<IItemPreparation> PreparationItems { get; set; }
+
+        public string Description { get; set; }
+
+        public string ServicePointDescription { get; }
+        public ItemsPreparationContext(IMealCourse mealCourse, IPreparationStation preparationStation, List<IItemPreparation> preparationItems)
+        {
+            //this.MealCourse = mealCourse;
+            this.PreparationStationIdentity = preparationStation.PreparationStationIdentity;
+            this.PreparationItems = preparationItems;
+            Description = preparationStation.Description;
+            ServicePointDescription = mealCourse.Meal.Session.ServicePoint.Description;
+        }
+        public ItemsPreparationContext()
+        {
+
+        }
+    }
+}
