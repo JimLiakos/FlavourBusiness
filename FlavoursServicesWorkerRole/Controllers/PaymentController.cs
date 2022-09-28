a76a13f5e24ed5d5ab70665bae7d49a3a41f9e02
Jim Liakos <Jim.Liakos.Msdn@hotmail.com>
2022-09-28 06:48:15 +0300

    jim

---
+++ FlavoursServicesWorkerRole/Controllers/PaymentController.cs
@@ -0,0 +1,45 @@
+﻿using Braintree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace FlavoursServicesWorkerRole.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PaymentController : ApiController
+    {
+
+        [Route("api/Payment/BraintreeClientToken")]
+
+        public string GetBraintreeClientToken()
+        {
+            return BraintreeGateway.ClientToken.Generate();
+        }
+
+
+        static BraintreeGateway _BraintreeGateway;
+        BraintreeGateway BraintreeGateway
+        {
+            get
+            {
+                if (_BraintreeGateway == null)
+                {
+
+                    _BraintreeGateway = new BraintreeGateway
+                    {
+                        Environment = Braintree.Environment.SANDBOX,
+                        MerchantId = "nyqdtchc77jt6z52",
+                        PublicKey = "cwy55c3by7zppkwq",
+                        PrivateKey = "010a3da02008d58cd90be24a1b7343c0"
+                    };
+                }
+                return _BraintreeGateway;
+            }
+        }
+    }
+}
