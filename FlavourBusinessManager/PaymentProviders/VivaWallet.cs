e9ec07565fa23bc22ed60cbbee46515a9166cb4f
Jim Liakos <Jim.Liakos.Msdn@hotmail.com>
2022-12-01 13:38:09 +0200

    JIM

---
+++ FlavourBusinessManager/PaymentProviders/VivaWallet.cs
@@ -0,0 +1,17 @@
+﻿using FinanceFacade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlavourBusinessManager.PaymentProviders
+{
+    public class VivaWallet
+    {
+        internal static void CreatePaymentOrder(Payment payment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
