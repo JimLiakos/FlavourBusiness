3d1b41ba907f6db474a446704fd54566f29ad2a3
Jim Liakos <Jim.Liakos.Msdn@hotmail.com>
2022-12-05 20:44:54 +0200

    jim

---
+++ FlavourBusinessManager/Webhookservice.cs
@@ -0,0 +1,108 @@
+﻿using DeviceMessageServer;
+using Microsoft.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WebhooksToLocalServer
+{
+    public class Webhookservice
+    {
+        public ServiceHost DeviceMessageServerServiceBusProxy { get; private set; }
+
+        public void Start(string serviceBusID)
+        {
+            //https://meridianmessagingservice.azurewebsites.net/api/WoltHook/NewOrder/CB848610-B5A5-4E21-8ED4-E6F470BA997A
+            try
+            {
+                //string serviceBusID = "CB848610-B5A5-4E21-8ED4-E6F470BA997A";
+                string sbUri = null;
+                string rootManageSharedAccessKeyValue = "f9tTGFYjW2Og77pIqt3pJRUbODquGh4NYVa4bCbSx9I=";
+                string servicebusName = "arionsoftware";
+
+                sbUri = ServiceBusEnvironment.CreateServiceUri("sb", servicebusName, serviceBusID).ToString();
+
+                DeviceMessageServerServiceBusProxy = new ServiceHost(typeof(DeviceMessageServer));
+                DeviceMessageServerServiceBusProxy.AddServiceEndpoint(typeof(IDeviceMessageServer), new NetTcpRelayBinding(), sbUri)
+                    .Behaviors.Add(new TransportClientEndpointBehavior
+                    {
+                        TokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider("RootManageSharedAccessKey", rootManageSharedAccessKeyValue)
+                    });
+
+                DeviceMessageServerServiceBusProxy.Open();
+            }
+            catch (AddressAccessDeniedException error)
+            {
+            }
+            catch (AddressAlreadyInUseException error)
+            {
+            }
+            catch (Exception error)
+            {
+                
+            }
+        }
+    }
+
+    [ServiceBehavior(Name = "DeviceMessageServer", Namespace = "https://samples.microsoft.com/ServiceModel/Relay/")]
+    class DeviceMessageServer : IDeviceMessageServer
+    {
+        static string _lock = "lock";
+
+        Task<string> PhoneCallerIdTask;
+        Task<string> PreparationStationTask;
+
+
+        public string SendCallBackEventMessage(string xmlMessage)
+        {
+
+            return "";
+        }
+
+        public string SendMessage(string xmlMessage)
+        {
+
+            var messageDoc = XDocument.Parse(xmlMessage);
+
+            string method = messageDoc.Root.Attribute("Method").Value;
+            string headersjSON=messageDoc.Root.Attribute("Headers").Value;
+            string content = messageDoc.Root.Attribute("Content").Value;
+            string webHookName= messageDoc.Root.Attribute("WebHookName").Value;
+            var headers=OOAdvantech.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(headersjSON);
+
+           var response= FlavourBusinessManager.ServicePointRunTime.ServicesContextRunTime.Current.WebHook(method, webHookName, headers, content);
+
+            headersjSON = OOAdvantech.Json.JsonConvert.SerializeObject(response.Headers);
+
+            var responseDoc = XDocument.Parse("<Main/>");
+            responseDoc.Root.SetAttributeValue("StatusCode", ((int)response.StatusCode).ToString());
+            responseDoc.Root.SetAttributeValue("Headers", headersjSON);
+            responseDoc.Root.SetAttributeValue("Content", response.Content);
+            return responseDoc.ToString();
+
+
+        }
+    }
+
+
+}
+namespace DeviceMessageServer
+{
+    [ServiceContract(Namespace = "urn:ps")]
+    public interface IDeviceMessageServer
+    {
+        [OperationContract]
+        string SendMessage(string xmlMessage);
+
+
+        [OperationContract]
+        string SendCallBackEventMessage(string xmlMessage);
+
+    }
+
+}
