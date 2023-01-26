3ced393eb27f1660eb68d7bd4b9791f71a6c17c1
mac <yiannis@arion.gr>
2023-01-26 10:29:20 +0200

    jim

--- DontWaitAppNS/DontWaitAppNS.iOS/AppDelegate.cs
+++ DontWaitAppNS/DontWaitAppNS.iOS/AppDelegate.cs
@@ -24,6 +24,7 @@ namespace DontWaitAppNS.iOS
             System.Diagnostics.Debug.WriteLine($"FCM Token: {fcmToken}");
         }
 
+
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
