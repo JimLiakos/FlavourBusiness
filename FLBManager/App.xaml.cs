32a72a58b44cbf27b539292d359d8d55374cf219
Jim Liakos <Jim.Liakos.Msdn@hotmail.com>
2024-01-23 11:38:39 +0200

    jim

--- FLBManager/App.xaml.cs
+++ FLBManager/App.xaml.cs
@@ -150,7 +150,7 @@ namespace FLBManager
 
             //Restore(@"F:\X-Drive\Source\OpenVersions\FlavourBusiness\FlavourBusinessApps\Backup", "DevStorage", "", "",true);
 
-            Restore(@"F:\myproject\terpo\OpenVersions\FlavourBusiness\FlavourBusinessApps\Backup", "DevStorage", "", "",true);
+            //Restore(@"F:\myproject\terpo\OpenVersions\FlavourBusiness\FlavourBusinessApps\Backup", "DevStorage", "", "",true);
             //Restore(@"F:\X-Drive\Source\OpenVersions\FlavourBusiness\Data\Backup", "DevStorage", "", "", true);
 
             //Restore(@"F:\NewPc\Azure blob storage\Backup", "DevStorage", "", "");
